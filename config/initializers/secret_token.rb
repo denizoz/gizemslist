a483f3ebcc177146c2f929cb3f77e7d87c7fe8ef
denizoz <denizoz@ubuntu.(none)>
2012-06-17 18:57:08 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gizemslist::Application.config.secret_token = '0932c1ac905a24b2b2755d4ffbf9a1d8d20277da7090148481ce89e5fb83025ddea4e1554a0d39d58bc2a41082463d38f413aaf8c8ac1c72d11e8ffc1868f176'
